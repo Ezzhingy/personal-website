48be61e325e5f10d74496e4cdf8510e4d3bdc945
Eugene Zhang <zhang.eug@gmail.com>
2023-12-23 21:28:01 -0500

    Rename banner

--- reusable/Banner.js
+++ reusable/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div className="absolute top-2 -right-2 w-10 bg-amber-300 dark:bg-blue-600  rotate-45 flex items-center justify-center">
+    <div className="absolute top-2 -right-2 w-10 bg-amber-300 dark:bg-blue-600 rotate-45 flex items-center justify-center">
       <p className="text-xs font-bold">NEW</p>
     </div>
   );
