c8683b829f9ba6371c588496900c23f3fa0729a4
Eugene Zhang <zhang.eug@gmail.com>
2024-02-24 11:02:50 -0500

    Add memlane project

--- functions/fetchPageData.js
+++ functions/fetchPageData.js
@@ -95,6 +95,17 @@ export function fetchHackthenorthData() {
   ];
 }
 
+export function fetchMemlaneData() {
+  return [
+    "/memlane-main.jpg",
+    "Hustle culture has engulfed us. The insatiable hunger of accomplishments looms over our everyday lives. With everything happening so quickly, sometimes we really just need to take a walk down memory lane—and that’s exactly what Memlane is for.",
+    "Memlane is a mobile app that helps you discover where memories are made. Memlane pins memories to the locations in which they were created, helping you trace the places you’ve been to and the experiences you’ve had. As you travel, you can also explore the memories that others have created nearby and how different spaces evolve over time.",
+    "Simply create an account, log in, and journey your experiences as you go! You can also view and react to other people’s memories as you come across them. Sharing and reliving memories has never been easier!",
+    "Our app was bootstrapped with Expo and Supabase. It was built using TypeScript & React Native and is accessible for both iOS and Android devices.",
+    "Memlane intends to polish up certain features (camera & map display). With a few more tweaks, we could potentially be bringing Memlane to the general public.",
+  ];
+}
+
 export function fetchDontLeafMeData() {
   return [
     "/dont-leaf-me-main.jpeg",
